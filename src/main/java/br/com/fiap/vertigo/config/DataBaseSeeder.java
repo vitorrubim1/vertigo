50bbfb461f339047a4d80ed8ae41389ae74c3c37
Exxtacsy <RM97068@fiap.com.br>
2023-10-23 14:11:15 -0300

    feat: "criando o data base seeder "

---
+++ src/main/java/br/com/fiap/vertigo/config/DataBaseSeeder.java
@@ -0,0 +1,60 @@
+package br.com.fiap.vertigo.config;
+
+import br.com.fiap.vertigo.model.Campeonato;
+import br.com.fiap.vertigo.model.Partida;
+import br.com.fiap.vertigo.model.Time;
+import br.com.fiap.vertigo.model.Usuario;
+import br.com.fiap.vertigo.repository.CampeonatoRepository;
+import br.com.fiap.vertigo.repository.PartidaRepository;
+import br.com.fiap.vertigo.repository.TimeRepository;
+import br.com.fiap.vertigo.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class DataBaseSeeder implements CommandLineRunner  {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+    @Autowired
+    TimeRepository timeRepository;
+    @Autowired
+    CampeonatoRepository campeonatoRepository;
+    @Autowired
+    PartidaRepository partidaRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        usuarioRepository.saveAll(
+          List.of(
+                  new Usuario(null, "Vitor","vitor@gmail.com", "1234" ),
+                  new Usuario(null, "Gustavo","gustavo@gmail.com", "1234" ),
+                  new Usuario(null, "Natan","natan@gmail.com", "1234" )
+          )
+        );
+        timeRepository.saveAll(
+                List.of(
+                        new Time(null, 35, 10, "https://www.imagem.com", "LOUD"),
+                        new Time(null, 30, 15, "https://www.imagem.com", "FLUXO"),
+                        new Time(null, 32, 13, "https://www.imagem.com", "RED CANIDS"),
+                        new Time(null, 28, 17, "https://www.imagem.com", "KEYD"),
+                        new Time(null, 33, 12, "https://www.imagem.com", "PAIN"),
+                        new Time(null, 26, 19, "https://www.imagem.com", "FURIA")
+
+                )
+        );
+        campeonatoRepository.saveAll(
+                List.of(
+                       new Campeonato(null, "CBLOL")
+                )
+        );
+        partidaRepository.saveAll(
+                List.of(
+                        new Partida(null,[{"id": 1}, {"id":2}],1,"2023-10-30T14:30:00")
+                )
+        );
+    }
+}
